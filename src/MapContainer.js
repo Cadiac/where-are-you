1d71150aad2d51a5198bc3ff24d03ae326599167
Jaakko Husso <jaakko.husso@futurice.com>
2017-06-11 11:30:31 +0300

    Scaffold the project from create-react-app and add MVP map

---
+++ src/MapContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+
+import './MapContainer.css';
+
+export default class MapContainer extends Component {
+  state = {
+    location: {
+      lat: 0,
+      lng: 0,
+    },
+  }
+
+  componentDidMount() {
+    this.updateLocation();
+  }
+
+  updateLocation() {
+    this.refs.map.leafletElement.locate({setView: true, maxZoom: 15});
+  }
+
+  handleLocationFound = e => {
+    this.setState({
+      hasLocation: true,
+      location: e.latlng,
+    });
+  }
+
+  render() {
+    return (
+      <Map
+        className="map-container"
+        center={this.state.location}
+        onLocationfound={this.handleLocationFound}
+        ref="map"
+      >
+        <TileLayer
+          url="https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FkaWFjIiwiYSI6ImNqM3JkNWZxbzAwNXIyd214aTFsdWZocGwifQ._Ar_1ePckcti9A_GIZHP6Q"
+        />
+        <Marker position={this.state.location}>
+          <Popup>
+            You are here
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
